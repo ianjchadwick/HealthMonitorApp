937f11c5c28767e38ecee2b887f9b38b11e483ca
Ian Chadwick <ianjchadwick@gmail.com>
2022-02-13 14:35:16 -0500

    Added logging for device_module.py

--- device_module.py
+++ device_module.py
@@ -1,5 +1,16 @@
 import json
+import logging
+
+logging.basicConfig(filename="logs/device_module.log", format='%(asctime)s - %(levelname)s - %(process)d - %(message)s',
+                    level=logging.INFO)
+
 
 def device_reading(json):
     """Module takes JSON as input from device, parses the data and uploads it to the correct database location"""
+    logging.info("Log Test")
     return
+
+
+if __name__ == "__main__":
+    test = "string"
+    device_reading(test)
