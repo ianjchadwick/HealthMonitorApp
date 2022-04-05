0837844098bf44b0b33f7122f810ed3470d52d1c
Ian Chadwick <ianjchadwick@gmail.com>
2022-04-05 14:09:20 -0400

    Fixed API issue

--- application.py
+++ application.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, abort
-import database
 
 header_text = '''
     <html>\n<head> <title>Device Module Flask Test</title> </head>\n<body>'''
--- test.py
+++ test.py
@@ -2,26 +2,28 @@ import requests
 import device_module
 import chat_module
 
-
-# BASE = "http://deviceapp-env.eba-jctvp42w.us-east-1.elasticbeanstalk.com/"
+# Test to use on localhost IP
 BASE = "http://127.0.0.1:5000/"
 
+# Loads a test JSON file from test_files directory and runs basic tests for device API
 test = "test_files/device_test.json"
 data = device_module.validate_device_json(test)
 print(data)
 device_id = str(data["_id"])
-print(device_id)
 
 response = requests.put(BASE + "device/" + device_id, data)
 print(response.json())
-"""
-response = requests.get(BASE + "device/" + device_id)
-print(response.json())
-response = requests.delete(BASE + "device/" + device_id)
-print(response)
+
 response = requests.get(BASE + "device/" + device_id)
 print(response.json())
 
+response = requests.delete(BASE + "device/" + device_id)
+print(response)
+
+response = requests.get(BASE + "device/" + device_id)
+print(response.json())
+
+# Loads a test JSON file from test_files directory and runs basic tests for chat message database API
 msgtest = "test_files/chat_test.json"
 msgdata = chat_module.validate_chat_json(msgtest)
 msg_id = str(msgdata["_id"])
@@ -37,4 +39,3 @@ print(response)
 
 response = requests.get(BASE + "chat/" + msg_id)
 print(response.json())
-"""
